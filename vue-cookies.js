fc8a07c6f86220550835c9fc2483a69913261a8a
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2020-07-27 19:04:36 +0200

    ES5

--- vue-cookies.js
+++ vue-cookies.js
@@ -1,66 +1,78 @@
-const defaultConfig = {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var defaultConfig = {
   expires: '1d',
   path: '; path=/',
   domain: '',
   secure: '',
-  sameSite: '; SameSite=Lax',
+  sameSite: '; SameSite=Lax'
 };
-
-export default {
+var _default = {
   // install of Vue
   reactive: null,
-  install(Vue) {
+  install: function install(Vue) {
     Vue.prototype.$cookies = this;
     Vue.$cookies = this;
-    const oldFetch = fetch;
+    var oldFetch = fetch;
     this.reactive = new Vue({
-      data() {
+      data: function data() {
         return {
-          docCookie: '',
+          docCookie: ''
         };
       },
       computed: {
         cookies: {
-          get() {
-            const c = this.docCookie.split('; ');
-            const cookies = {};
+          get: function get() {
+            var c = this.docCookie.split('; ');
+            var cookies = {};
 
-            for (let i = c.length - 1; i >= 0; i--) {
-              const k = c[i].split('=');
+            for (var i = c.length - 1; i >= 0; i--) {
+              var k = c[i].split('=');
+
               try {
                 cookies[k[0]] = JSON.parse(k[1]);
               } catch (e) {
                 cookies[k[0]] = k[1];
               }
             }
+
             return cookies;
           },
-          set(cookie) {
+          set: function set(cookie) {
             document.cookie = cookie;
             this.refresh();
-          },
-        },
+          }
+        }
       },
-      created() {
+      created: function created() {
         this.refresh();
       },
       methods: {
-        refresh() {
+        refresh: function refresh() {
           this.docCookie = document.cookie;
-        },
-      },
+        }
+      }
     });
-    const resetCookies = this.reactive.refresh.bind(this);
-    // Listen for any http request to update cookies when they are done
+    var resetCookies = this.reactive.refresh.bind(this); // Listen for any http request to update cookies when they are done
     // eslint-disable-next-line no-global-assign
-    fetch = function() {
+
+    fetch = function fetch() {
       return oldFetch.apply(this, arguments).then(resetCookies());
     };
-    const open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-      setTimeout(() => {
-        const old = this.onreadystatechange;
-        this.onreadystatechange = function() {
+
+    var open = XMLHttpRequest.prototype.open;
+
+    XMLHttpRequest.prototype.open = function () {
+      var _this = this;
+
+      setTimeout(function () {
+        var old = _this.onreadystatechange;
+
+        _this.onreadystatechange = function () {
           resetCookies();
           old.apply(this, arguments);
         };
@@ -68,98 +80,107 @@ export default {
       return open.apply(this, arguments);
     };
   },
-  config(expireTimes, path, domain, secure, sameSite) {
+  config: function config(expireTimes, path, domain, secure, sameSite) {
     defaultConfig.expires = expireTimes || '1d';
     defaultConfig.path = path ? '; path=' + path : '; path=/';
     defaultConfig.domain = domain ? '; domain=' + domain : '';
     defaultConfig.secure = secure ? '; Secure' : '';
     defaultConfig.sameSite = sameSite ? '; SameSite=' + sameSite : '; SameSite=Lax';
   },
-  get(key) {
+  get: function get(key) {
     return key !== undefined ? this.reactive.cookies[key] : this.reactive.cookies;
   },
-  set(key, value, expireTimes, path, domain, secure, sameSite) {
+  set: function set(key, value, expireTimes, path, domain, secure, sameSite) {
     if (/^(?:expires|max-age|path|domain|secure|SameSite)$/i.test(key)) {
       throw new Error('Cookie key name illegality, Cannot be set to ["expires","max-age","path","domain","secure","SameSite"]\t current key name: ' + key);
-    }
-    // support json object
+    } // support json object
+
+
     if (value && value.constructor === Object) {
       value = JSON.stringify(value);
     }
-    let _expires = '';
+
+    var _expires = '';
     expireTimes = expireTimes === undefined ? defaultConfig.expires : expireTimes;
+
     if (expireTimes) {
       switch (expireTimes.constructor) {
         case Number:
-          if (expireTimes === Infinity || expireTimes === -1) _expires = '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
-          else _expires = '; max-age=' + expireTimes;
+          if (expireTimes === Infinity || expireTimes === -1) _expires = '; expires=Fri, 31 Dec 9999 23:59:59 GMT';else _expires = '; max-age=' + expireTimes;
           break;
+
         case String:
           if (/^(?:\d+(y|m|d|h|min|s))$/i.test(expireTimes)) {
             // get capture number group
-            const _expireTime = expireTimes.replace(/^(\d+)(?:y|m|d|h|min|s)$/i, '$1');
-            // get capture type group , to lower case
+            var _expireTime = expireTimes.replace(/^(\d+)(?:y|m|d|h|min|s)$/i, '$1'); // get capture type group , to lower case
+
+
             switch (expireTimes.replace(/^(?:\d+)(y|m|d|h|min|s)$/i, '$1').toLowerCase()) {
               // Frequency sorting
               case 'm':
                 _expires = '; max-age=' + +_expireTime * 2592000;
-                break; // 60 * 60 * 24 * 30
+                break;
+              // 60 * 60 * 24 * 30
+
               case 'd':
                 _expires = '; max-age=' + +_expireTime * 86400;
-                break; // 60 * 60 * 24
+                break;
+              // 60 * 60 * 24
+
               case 'h':
                 _expires = '; max-age=' + +_expireTime * 3600;
-                break; // 60 * 60
+                break;
+              // 60 * 60
+
               case 'min':
                 _expires = '; max-age=' + +_expireTime * 60;
-                break; // 60
+                break;
+              // 60
+
               case 's':
                 _expires = '; max-age=' + _expireTime;
                 break;
+
               case 'y':
                 _expires = '; max-age=' + +_expireTime * 31104000;
-                break; // 60 * 60 * 24 * 30 * 12
+                break;
+              // 60 * 60 * 24 * 30 * 12
+
               default:
                 throw new Error('unknown exception of "set operation"');
             }
           } else {
             _expires = '; expires=' + expireTimes;
           }
+
           break;
+
         case Date:
           _expires = '; expires=' + expireTimes.toUTCString();
           break;
       }
     }
-    this.reactive.cookies =
-      encodeURIComponent(key) + '=' + encodeURIComponent(value) +
-      _expires +
-      (domain ? '; domain=' + domain : defaultConfig.domain) +
-      (path ? '; path=' + path : defaultConfig.path) +
-      (secure === undefined ? defaultConfig.secure : secure ? '; Secure' : '') +
-      (sameSite === undefined ? defaultConfig.sameSite : (sameSite ? '; SameSite=' + sameSite : ''));
+
+    this.reactive.cookies = encodeURIComponent(key) + '=' + encodeURIComponent(value) + _expires + (domain ? '; domain=' + domain : defaultConfig.domain) + (path ? '; path=' + path : defaultConfig.path) + (secure === undefined ? defaultConfig.secure : secure ? '; Secure' : '') + (sameSite === undefined ? defaultConfig.sameSite : sameSite ? '; SameSite=' + sameSite : '');
     return this;
   },
-  remove(key, path, domain) {
+  remove: function remove(key, path, domain) {
     if (!key || !this.isKey(key)) {
       return false;
     }
-    this.reactive.cookies = encodeURIComponent(key) +
-      '=; expires=Thu, 01 Jan 1970 00:00:00 GMT' +
-      (domain ? '; domain=' + domain : defaultConfig.domain) +
-      (path ? '; path=' + path : defaultConfig.path) +
-      '; SameSite=Lax';
+
+    this.reactive.cookies = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT' + (domain ? '; domain=' + domain : defaultConfig.domain) + (path ? '; path=' + path : defaultConfig.path) + '; SameSite=Lax';
     return this;
   },
-  isKey(key) {
+  isKey: function isKey(key) {
     return key in this.reactive.cookies;
   },
-  keys() {
+  keys: function keys() {
     return Object.keys(this.reactive.cookies);
   },
-  refresh() {
+  refresh: function refresh() {
     this.reactive.refresh();
-
     return this;
-  },
+  }
 };
+exports.default = _default;
